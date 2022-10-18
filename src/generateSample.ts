7840c251596c3b605357fec6fa7552741e35e153
percegaroli <bruno.percegaroli@gmail.com>
2022-10-18 18:38:31 -0300

    change sample quantity

--- src/generateSample.ts
+++ src/generateSample.ts
@@ -1,17 +1,29 @@
 import { DEFAULT_SAMPLE_CONFIG, SampleConfig } from "./samplesConfig";
 
-const generateSamples = ({ maxValue, samplesQuantity}: Omit<SampleConfig, 'quantityPerSample'>) => {
-    return new Array(samplesQuantity)
-        .fill(undefined)
-        .map(() => Math.ceil(Math.random() * maxValue))
-}
+const generateSamples = ({
+  maxValue,
+  samplesQuantity,
+}: Omit<SampleConfig, "quantityPerSample">) => {
+  return new Array(samplesQuantity)
+    .fill(undefined)
+    .map(() => Math.ceil(Math.random() * maxValue));
+};
 
-const generateMultipleSamples = ({ samplesQuantity, maxValue, quantityPerSample }: SampleConfig) => {
-    return new Array(quantityPerSample)
-        .fill(undefined)
-        .map(() => generateSamples({samplesQuantity, maxValue}))
-} 
+const generateMultipleSamples = ({
+  samplesQuantity,
+  maxValue,
+  quantityPerSample,
+}: SampleConfig) => {
+  return new Array(quantityPerSample)
+    .fill(undefined)
+    .map(() => generateSamples({ samplesQuantity, maxValue }));
+};
 
 export const SAMPLES = Array(20)
-    .fill(undefined)
-    .map((_, index) => generateMultipleSamples({...DEFAULT_SAMPLE_CONFIG, samplesQuantity: Math.pow(2, index + 1 )}))
+  .fill(undefined)
+  .map((_, index) =>
+    generateMultipleSamples({
+      ...DEFAULT_SAMPLE_CONFIG,
+      samplesQuantity: Math.pow(2, index + 1),
+    })
+  );
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { differenceInMilliseconds } from "date-fns";
-import { stdev } from 'stats-lite'
+import { stdev } from "stats-lite";
 import { SAMPLES } from "./generateSample";
 import { heapSort } from "./sortAlrorithms/heapSort";
 import { insertionSort } from "./sortAlrorithms/insertionSort";
@@ -8,58 +8,64 @@ import { quickSort } from "./sortAlrorithms/quickSort";
 import { selectionSort } from "./sortAlrorithms/selectionSort";
 import { calculateArrayMean } from "./utils/calculateArrayMean";
 
-type OrdenationFn = (numbers: Array<number>) => Array<number>
+type OrdenationFn = (numbers: Array<number>) => Array<number>;
 
 const ordenationFunctions = [
-    {
-        name: 'InsertionSort',
-        sortFn: insertionSort,
-        implemented: false,
-    },
-    {
-        name: 'HeapSort',
-        sortFn: heapSort,
-        implemented: true
-    },
-    {
-        name: 'QuickSort',
-        sortFn: quickSort,
-        implemented: true,
-    },
-    {
-        name: 'SelectioSort',
-        sortFn: selectionSort,
-        implemented: true,
-    },
-    {
-        name: 'MergeSort',
-        sortFn: mergeSort,
-        implemented: true
-    },
-]
+  {
+    name: "InsertionSort",
+    sortFn: insertionSort,
+    implemented: true,
+  },
+  {
+    name: "HeapSort",
+    sortFn: heapSort,
+    implemented: true,
+  },
+  {
+    name: "QuickSort",
+    sortFn: quickSort,
+    implemented: true,
+  },
+  {
+    name: "SelectionSort",
+    sortFn: selectionSort,
+    implemented: true,
+  },
+  {
+    name: "MergeSort",
+    sortFn: mergeSort,
+    implemented: true,
+  },
+];
 
-const getOrdenationTimeInMiliseconds = (numbers: Array<number>, ordenationFn: OrdenationFn) => {
-    const initialTime = new Date();
-    ordenationFn([...numbers]);
-    const finalTime = new Date();
-    return differenceInMilliseconds(finalTime, initialTime)
-}
+const getOrdenationTimeInMiliseconds = (
+  numbers: Array<number>,
+  ordenationFn: OrdenationFn
+) => {
+  const initialTime = new Date();
+  ordenationFn([...numbers]);
+  const finalTime = new Date();
+  return differenceInMilliseconds(finalTime, initialTime);
+};
 
 ordenationFunctions
-    .filter(({implemented}) => implemented)
-    .map(({ sortFn, name, }) => ({
-        name,
-        results: SAMPLES.map(sampleGroup => {
-            const durations = sampleGroup.map(sample => getOrdenationTimeInMiliseconds(sample, sortFn))
-            return {
-                'tamanho arrays': sampleGroup[0].length,
-                'tempo medio ordenacao (ms)': calculateArrayMean(durations),
-                'desvio padrao': stdev(durations) 
-            }}
-        )
-    }))
-    .forEach(({ name, results}) => {
-        console.log(`Algoritmo: ${name}`);
-        console.table(results);
-    })
- 
+  .filter(({ implemented }) => implemented)
+  .map(({ sortFn, name }) => ({
+    name,
+    results: SAMPLES.map((sampleGroup) => {
+      const durations = sampleGroup.map((sample) => {
+        const result = getOrdenationTimeInMiliseconds(sample, sortFn);
+        return result;
+      });
+
+      return {
+        "tamanho arrays": sampleGroup[0].length,
+        "tempo medio ordenacao (ms)": calculateArrayMean(durations),
+        "desvio padrao": stdev(durations),
+      };
+    }),
+  }))
+  .forEach(({ name, results }) => {
+    console.log(`Algoritmo: ${name}`);
+    console.table(results);
+  });
